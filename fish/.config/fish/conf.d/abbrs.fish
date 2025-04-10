7c9d39a31c4be250fe037e43491f0df5f65f173f
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-04-10 11:14:32 +0800

    Touchup git
    Move txt file to md due to local-stow-ignore rules
    Add dense to `lf` abbr in fish

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,5 +1,5 @@
 abbr -a -- kssh 'kitty +kitten ssh --kitten="askpass=native" --kitten="login_shell=fish"'
-abbr -a --position anywhere --command git -- lf 'log --graph --decorate --pretty=fuller'
+abbr -a --position anywhere --command git -- lf 'log --graph --decorate --dense --pretty=fuller'
 abbr -a --position anywhere --command git -- oo --oneline
 abbr -a --position anywhere --command git -- aa --all
 abbr -a --position anywhere --command git -- rbc 'rebase --committer-date-is-author-date --update-refs --autostash --autosquash'
