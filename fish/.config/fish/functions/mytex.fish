87346da19816bab7d9bad4818018ca5f962aa11d
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2026-01-20 21:18:22 +0800

    Revamp mytex function in fish

--- fish/.config/fish/functions/mytex.fish
+++ fish/.config/fish/functions/mytex.fish
@@ -1,4 +1,6 @@
 function mytex
     set -l bname (basename $argv | cut -d . -f1)
-    pdflatex $bname && biber $bname && pdflatex $bname && pdflatex $bname
+    if not latexmk -pdf -pdflatex="pdflatex -interactive=nonstopmode" "$bname.tex"
+        pdflatex $bname && biber $bname && pdflatex $bname && pdflatex $bname
+    end
 end
